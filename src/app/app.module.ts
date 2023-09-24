7c0507c4058e10a2e8292b65f8e43fd3cf8e7f04
Emmet Spencer <emmetwormbox6@gmail.com>
2023-09-23 23:44:36 -0400

    Fixed??? PLZ

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,9 @@ import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HomeComponent } from './home/home.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-<<<<<<< HEAD
 import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
 
-=======
 import { NavbarComponent } from './navbar/navbar.component';
->>>>>>> 190235d5f5cb7ca14c7b0a746dd3ee3e317ffab4
 
 @NgModule({
   declarations: [
@@ -28,13 +25,8 @@ import { NavbarComponent } from './navbar/navbar.component';
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD
     AppRoutingModule,
     HttpClientModule
-=======
-    AppRoutingModule
-    
->>>>>>> 190235d5f5cb7ca14c7b0a746dd3ee3e317ffab4
   ],
   providers: [],
   bootstrap: [AppComponent]
