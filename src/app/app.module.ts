88a2dc7a4bd22e146c5b8b061ce047dd94c25d57
Zaikoa <jpforzanojr@gmail.com>
2023-09-24 01:01:02 -0400

    removed cache and added it to git ignore

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-<<<<<<< HEAD
 import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
-=======
-import { HomeComponent } from './home/home.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
-
-import { NavbarComponent } from './navbar/navbar.component';
->>>>>>> 61364d10d6d432f1adaadb625aef26f596ddcc0d
 
 @NgModule({
   declarations: [
@@ -20,14 +12,8 @@ import { NavbarComponent } from './navbar/navbar.component';
     LoginComponent
   ],
   imports: [
-<<<<<<< HEAD
     AppRoutingModule,
     BrowserModule    
-=======
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
->>>>>>> 61364d10d6d432f1adaadb625aef26f596ddcc0d
   ],
   providers: [],
   bootstrap: [AppComponent]
